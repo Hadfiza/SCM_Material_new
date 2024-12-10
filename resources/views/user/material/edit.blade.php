3c8827247a2342bf18f5978974f35551b7385ee3
Hadfiza <hadfizaiza@gmail.com>
2024-12-11 02:35:05 +0800

    User pengiriman

--- resources/views/user/material/edit.blade.php
+++ resources/views/user/material/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.app')
+@extends('layouts.app')
 @section('title', 'Edit Material')
 
 @section('content')
--- resources/views/user/pengiriman/index.blade.php
+++ resources/views/user/pengiriman/index.blade.php
@@ -1,72 +1,46 @@
 @extends('layouts.app')
+@section('title', 'Pengiriman')
+@section('custom-css')
+  <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+@section('navbar', 'Pengiriman')
 
 @section('content')
-<html>
-<head>
-    <title>
-        Daftar Pengiriman
-    </title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
-    <style>
-        body {
-            font-family: 'Roboto', sans-serif;
-        }
-    </style>
-</head>
-<body class="bg-gray-100">
-<main class="container mx-auto mt-8">
-    <div class="bg-white shadow-md rounded-lg p-6">
-        <div class="flex justify-between items-center mb-4">
-            <h2 class="text-xl font-bold">
-                Daftar Pengiriman
-            </h2>
-            <p class="text-lg font-bold">
-                Jumlah Pengiriman: <span class="text-blue-600">{{ $totalPengiriman }}</span>
-            </p>
+    <!-- Content -->
+    <div>
+        <div class="d-flex justify-content-between mt-3 mb-2">
+            <!-- Search -->
+            <form action="{{ route('admin.pengiriman') }}" method="GET" class="d-flex">
+                <input type="text" name="search" placeholder="Search Order ID" class="form-control me-2" value="{{ request('search') }}">
+                <button type="submit" class="btn btn-primary">Search</button>
+            </form>
         </div>
-        <div class="overflow-x-auto">
-            @if($pengiriman->isEmpty())
-                <p>Tidak ada pengiriman yang tersedia.</p>
-            @else
-                <table class="min-w-full bg-white">
-                    <thead>
+        <!-- Pengiriman -->
+        <table class="table table-bordered table-responsive">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Order ID</th>
+                    <th>Tanggal Kirim</th>
+                    <th>Tanggal Selesai</th>
+                    <th>Status Pengiriman</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($pengiriman as $item)
                     <tr>
-                        <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
-                            ID Pengiriman
-                        </th>
-
-                        <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
-                            Tanggal Kirim
-                        </th>
-                        <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
-                            Tanggal Kedatangan
-                        </th>
-                        <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
-                            Order ID
-                        </th>
-                        <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
-                            Status Pengiriman
-                        </th>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->order_id }}</td>
+                        <td>{{ \Carbon\Carbon::parse($item->tanggal_kirim)->format('d-m-Y H:i:s') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($item->tanggal_selesai)->format('d-m-Y H:i:s') }}</td>
+                        <td>{{ $item->status_pengiriman }}</td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($pengiriman as $item)
-                        <tr>
-                            <td class="py-2 px-4 border-b border-gray-200">{{ $item->id }}</td>
-                            <td class="py-2 px-4 border-b border-gray-200">{{ $item->tanggal_kirim }}</td>
-                            <td class="py-2 px-4 border-b border-gray-200">{{ $item->tanggal_selesai }}</td>
-                            <td class="py-2 px-4 border-b border-gray-200">{{ $item->order_id }}</td>
-                            <td class="py-2 px-4 border-b border-gray-200">{{ $item->status_pengiriman }}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            @endif
-        </div>
+                @empty
+                    <tr>
+                        <td colspan="6" class="text-center">Tidak ada data pengiriman ditemukan.</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
     </div>
-</main>
-</body>
-</html>
 @endsection
